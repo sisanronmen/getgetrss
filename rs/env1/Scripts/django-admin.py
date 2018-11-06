4e908af0ee57bba7958c5c29a6eea392f0fb94ab
sisanronmen <41989018+sisanronmen@users.noreply.github.com>
2018-11-06 11:51:35 +0900

    不要ファイル削除

--- rs/env1/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\azco-virt-02\Desktop\rs\env1\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- rs/mybook/cms/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from cms.models import priRSS
-
-
-class rssForm(ModelForm):
-    """書籍のフォーム"""
-    class Meta:
-        model = priRSS
-        fields = ('url', 'siteName', 'siteDetail', )
